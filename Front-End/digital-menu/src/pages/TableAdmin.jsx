b8fbf951a3f71e68c731d9b5177cf599c8878ea1
anazil <anazilthottunghal@gmail.com>
2026-02-09 20:27:19 +0530

    Fix table link to use Vercel URL

--- Front-End/digital-menu/src/pages/TableAdmin.jsx
+++ Front-End/digital-menu/src/pages/TableAdmin.jsx
@@ -279,7 +279,7 @@ function TableAdmin() {
                       
                       <div className="table-link">
                         <a
-                          href={`http://localhost:5173/table/${table.table_number}/${userId}`}
+                          href={`https://cliqeat.vercel.app/table/${table.table_number}/${userId}`}
                           target="_blank"
                           rel="noopener noreferrer"
                           className="view-link"
